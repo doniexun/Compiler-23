eb781e631c755d34b04eeadf1d53b6d79125892f
clancy <claudchen7990@gmail.com>
2014-06-10 21:13:45 +0800

    delete old file

--- operator.c
+++
@@ -1,9 +0,0 @@
-#include "operator.h"
-
-static char *OPERATOR[] = {
-	"+", "-", "*", "/",
-	"=",
-	".NOT.", ".AND.", ".OR.", "GT", "GE", "LT", "LE", ".EQV.", ".NEQV."
-};
-
-char *operatorStr(enum operator op) { return OPERATOR[op]; }
